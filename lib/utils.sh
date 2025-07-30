d8f0c0e09c7079ddb3e4deb3c4357952928b31df
Ahzyuan <hzy_dev@yeah.net>
2025-07-30 17:45:04 +0800

    fix: gray color display

--- lib/utils.sh
+++ lib/utils.sh
@@ -8,7 +8,7 @@ readonly RED='\033[0;31m'
 readonly GREEN='\033[0;32m'
 readonly YELLOW='\033[1;33m'
 readonly BLUE='\033[0;34m'
-readonly GRAY='\033[0;37m'
+readonly GRAY='\033[0;90m'
 readonly NC='\033[0m' # No Color
 
 # Global variables for cleanup
